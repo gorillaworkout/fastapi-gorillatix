d198f2b47de9fce9537eac645ddaa447b1def8c7
Bayu Darmawan <darmawanbayu1@gmail.com>
2025-07-06 17:04:13 +0700

    update

--- app/main.py
+++ app/main.py
@@ -10,7 +10,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "http://127.0.0.1:3000"
+        "http://127.0.0.1:3000",
+        "https://gorillatix-v2.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
