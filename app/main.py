0c79bcc53465f871f031958726f95585e8593f59
Bayu Darmawan <darmawanbayu1@gmail.com>
2025-07-06 17:08:36 +0700

    delete slash

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
-        "https://gorillatix-v2.vercel.app/"
+        "https://gorillatix-v2.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
